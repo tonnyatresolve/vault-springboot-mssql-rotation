3bb070e28032c4924cfd26b936372d19a4ade042
Tonny <tonny@Tonnys-MacBook-Air-M1.local>
2023-05-04 10:06:20 +0800

    initial commit

---
+++ src/main/java/com/knf/dev/demo/MyConfiguration.java
@@ -0,0 +1,32 @@
+package com.knf.dev.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Mark Paluch
+ */
+@ConfigurationProperties("spring.datasource")
+public class MyConfiguration {
+
+  //private String username;
+
+  private String password;
+
+  /*
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  */
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
